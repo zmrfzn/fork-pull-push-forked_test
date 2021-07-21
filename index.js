f6733521495a62c32479eae71465c274a4ffb272
Zameer Fouzan <zmrfzn@gmail.com>
2021-07-21 17:39:08 +0530

    express starter

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import the top-level function of express
+const express = require('express');
+
+// Creates an Express application using the top-level function
+const app = express();
+
+// Define port number as 3000
+const port = 3000;
+
+// Routes HTTP GET requests to the specified path "/" with the specified callback function
+app.get('/', function(request, response) {
+  response.send('Hello, World!');
+});
+
+// Make the app listen on port 3000
+app.listen(port, function() {
+  console.log('Server listening on http://localhost:' + port);
+});
+
+exports = {
+    app
+}
